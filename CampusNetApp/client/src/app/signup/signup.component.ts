40292f17848f6d15c877d79ca71c39720f4cf717
rohanraj07 <rohsoterminator@gmail.com>
2017-11-28 10:51:11 -0500

    Sign up modified for course details page

--- CampusNetApp/client/src/app/signup/signup.component.ts
+++ CampusNetApp/client/src/app/signup/signup.component.ts
@@ -3,50 +3,50 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService, UserService } from '../_services/index';
 
-import {User} from '../_models/user';
+import { User } from '../_models/user';
 
 @Component({
-   selector: 'app-signup',
-   templateUrl: './signup.component.html',
-   styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-   model: any = {};
-   test: Date = new Date();
-   loading = false;
+    model: any = {};
+    test: Date = new Date();
+    loading = false;
 
-   majors = ['Computer Engineering', 'Computer Science', 'Mechanical Engineering', 'Plastics Engineering'];
-
-
-   itemList = [
-    { "id":1, "itemName": "IWS", "category": "Computer Science" },
-    { "id":2, "itemName": "HCI", "category": "Computer Science" },
-    { "id":3, "itemName": "OS", "category": "Computer Engineering" },
-    { "id":4, "itemName": "Network", "category": "Computer Engineering" },
-    { "id":5, "itemName": "Chemistry", "category": "Plastics Engineering" },
-    { "id":6, "itemName": "Robotics", "category": "Mechanical Engineering" }
-   ];
-   prevselectedItems = [];
-   currentselectedItems = [];
-   settings = {};
-
-   user = new User();
-
-   prevCourses:string[] = [];
-   currentCourses:string[] = [];
-   userForm: FormGroup;
+    majors = ['Computer Engineering', 'Computer Science', 'Mechanical Engineering', 'Plastics Engineering'];
 
 
+    itemList = [
+        { "id": 1, "itemName": "IWS", "category": "Computer Science" },
+        { "id": 2, "itemName": "HCI", "category": "Computer Science" },
+        { "id": 3, "itemName": "OS", "category": "Computer Engineering" },
+        { "id": 4, "itemName": "Network", "category": "Computer Engineering" },
+        { "id": 5, "itemName": "Chemistry", "category": "Plastics Engineering" },
+        { "id": 6, "itemName": "Robotics", "category": "Mechanical Engineering" }
+    ];
+    prevselectedItems = [];
+    currentselectedItems = [];
+    settings = {};
 
-   constructor(private router: Router,
-       private userService: UserService,
-       private alertService: AlertService,
-    private fb: FormBuilder) {
+    user = new User();
+
+    prevCourses: string[] = [];
+    currentCourses: string[] = [];
+    userForm: FormGroup;
+
+
+
+    constructor(private router: Router,
+        private userService: UserService,
+        private alertService: AlertService,
+        private fb: FormBuilder) {
         this.createForm();
-     }
+    }
 
 
-     createForm() {
+    createForm() {
         this.userForm = this.fb.group({
             name: '',
             email: ['', Validators.required],
@@ -61,16 +61,16 @@ export class SignupComponent implements OnInit {
 
 
 
-   ngOnInit() {
-    this.settings = {
-        text: "Select Fields",
-        selectAllText: 'Select All',
-        unSelectAllText: 'UnSelect All',
-        searchPlaceholderText: 'Search Fields',
-        enableSearchFilter: true,
-        badgeShowLimit: 4,
-        groupBy: "category"
-    };
+    ngOnInit() {
+        this.settings = {
+            text: "Select Fields",
+            selectAllText: 'Select All',
+            unSelectAllText: 'UnSelect All',
+            searchPlaceholderText: 'Search Fields',
+            enableSearchFilter: true,
+            badgeShowLimit: 4,
+            groupBy: "category"
+        };
     }
 
     onItemSelect(item: any) {
@@ -89,12 +89,12 @@ export class SignupComponent implements OnInit {
     }
     showModel() {
         //console.log(this.selectedItems);
-      }
+    }
     changeData() {
         this.prevselectedItems = [];
-      }
+    }
 
-      onItemSelect1(item1: any) {
+    onItemSelect1(item1: any) {
         //console.log(item);
         //console.log(this.selectedItems);
     }
@@ -110,50 +110,50 @@ export class SignupComponent implements OnInit {
     }
     showModel1() {
         //console.log(this.selectedItems);
-      }
+    }
     changeData1() {
         this.currentselectedItems = [];
-      }
+    }
 
 
-   register() {
-       this.loading = true;
-       //console.log(this.model);
-       //console.log(this.selectedItems);
-       
-       for(let result of this.prevselectedItems){
-          this.prevCourses.push(result.itemName);
-       }
+    register() {
+        this.loading = true;
+        //console.log(this.model);
+        //console.log(this.selectedItems);
 
-       this.model.prevcourses = this.prevCourses;
+        for (let result of this.prevselectedItems) {
+            this.prevCourses.push(result.id);
+        }
 
-       for(let result1 of this.currentselectedItems){
-        this.currentCourses.push(result1.itemName);
-     }
+        this.model.prevcourses = this.prevCourses;
 
-     this.model.currentcourses = this.currentCourses;
+        for (let result1 of this.currentselectedItems) {
+            this.currentCourses.push(result1.id);
+        }
+
+        this.model.currentcourses = this.currentCourses;
 
 
-     console.log(this.model);
+        console.log(this.model);
 
-       this.userService.create(this.model)
-           .subscribe(
-           data => {
-               console.log(data.json());
+        this.userService.create(this.model)
+            .subscribe(
+            data => {
+                console.log(data.json());
 
-               if (data.json().success === true) {
-                   this.alertService.success('Registration successful', true);
-                   this.router.navigate(['/landing']);
-               } else {
-                   this.alertService.error('Username Already exist', true);
-                   //this.router.navigate(['/landing']);
-               }
+                if (data.json().success === true) {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/landing']);
+                } else {
+                    this.alertService.error('Username Already exist', true);
+                    //this.router.navigate(['/landing']);
+                }
 
-           },
-           error => {
-               this.alertService.error(error);
-               this.loading = false;
-           });
-   }
-   
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+    }
+
 }
